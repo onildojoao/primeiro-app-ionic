7cd0a0c347c0749c4ed9be158de138d7745fba30
onildojoaocosta <onildojoaocosta@gmail.com>
2018-04-30 23:42:43 -0300

    Aplicação antes de usar json

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -14,11 +14,23 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'feed.html',
 })
 export class FeedPage {
-
+  public objeto_feed = {
+    titulo: "Titulo feed",
+    data: "05/05/05",
+    descricao: "Descricao do texto",
+    qnt_likes: "10 likes",
+    qnt_comments: "5 likes",
+    time_comment: "10h atras"
+  }
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
+  public nome_usuario:string="Teste codigo";
 
+  public somaDoisNumeros(n1:number, n2:number): void {
+  alert(n1 + n2);
+}
   ionViewDidLoad() {
+    //this.somaDoisNumeros(10, 10);
     console.log('ionViewDidLoad FeedPage');
   }
 
